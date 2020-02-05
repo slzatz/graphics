273e0e4cf0f5536eff855b9356faebab5170a8b0
slzatz <slzatz@gmail.com>
2020-02-05 16:27:52 -0500

    added the openglframework from thecherno

---
+++ thecherno/openglframework/CMakeFiles/thecherno.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thecherno.dir/OpenGL/src/Application.cpp.o"
+  "CMakeFiles/thecherno.dir/OpenGL/src/IndexBuffer.cpp.o"
+  "CMakeFiles/thecherno.dir/OpenGL/src/Renderer.cpp.o"
+  "CMakeFiles/thecherno.dir/OpenGL/src/Shader.cpp.o"
+  "CMakeFiles/thecherno.dir/OpenGL/src/Texture.cpp.o"
+  "CMakeFiles/thecherno.dir/OpenGL/src/VertexArray.cpp.o"
+  "CMakeFiles/thecherno.dir/OpenGL/src/VertexBuffer.cpp.o"
+  "CMakeFiles/thecherno.dir/OpenGL/src/tests/Test.cpp.o"
+  "CMakeFiles/thecherno.dir/OpenGL/src/tests/TestClearColor.cpp.o"
+  "CMakeFiles/thecherno.dir/OpenGL/src/tests/TestTexture2D.cpp.o"
+  "CMakeFiles/thecherno.dir/OpenGL/src/vendor/imgui/imgui.cpp.o"
+  "CMakeFiles/thecherno.dir/OpenGL/src/vendor/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/thecherno.dir/OpenGL/src/vendor/imgui/imgui_impl_glfw_gl3.cpp.o"
+  "CMakeFiles/thecherno.dir/OpenGL/src/vendor/stb_image/stb_image.cpp.o"
+  "thecherno"
+  "thecherno.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thecherno.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
