66f229e943bec1c6be274cad94140259285f8c6e
slzatz <slzatz@gmail.com>
2020-02-25 15:17:56 -0500

    thecherno OpenGL cleanup

--- thecherno/openglframework/CMakeFiles/Makefile.cmake
+++ thecherno/openglframework/CMakeFiles/Makefile.cmake
@@ -62,5 +62,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/thecherno.dir/DependInfo.cmake"
+  "CMakeFiles/test.dir/DependInfo.cmake"
   )
